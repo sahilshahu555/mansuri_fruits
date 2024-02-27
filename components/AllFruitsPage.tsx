7c73ab058a935591858a43d2a443c1de7718c0d5
sahilshahu555 <sahilshahu555@gmail.com>
2024-02-27 15:55:20 +0530

    created Fruits App

---
+++ components/AllFruitsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllFruitsPage = () => {
+  return (
+    <div>AllFruitsPage</div>
+  )
+}
+
+export default AllFruitsPage
---
+++ components/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>card</div>
+  )
+}
+
+export default Card
